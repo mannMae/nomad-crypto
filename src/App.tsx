39449da47360d4247a49ae96a7bcd9ad26092297
Jaeman Kim <daga4242@gmail.com>
2022-09-03 20:06:16 +0900

    styled & typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Circle from './Circle';
+
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor='teal'/>
+      <Circle bgColor='tomato'/>
+    </div>
+  );
+}
+
+export default App;
